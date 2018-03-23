3d03876f0a9791724a56ea2ae1f4314011478012
Romain Maneschi <romain.maneschi@itk.fr>
2018-03-23 14:47:32 +0100

    fix bug
    * if page has not hiddenFilter don't listen hashchange
    * fix card has link

--- static/js/cardFilter.js
+++ static/js/cardFilter.js
@@ -1,11 +1,19 @@
 (function() {
-    var check = function check() {
+    var check = function check(hiddenFilter) {
         var hash = '';
         if(window.location.hash) {
             hash = window.location.hash.split('#')[1];
-            document.getElementsByClassName('hiddenFilter')[0].style.opacity = '1';
+            for(var i in hiddenFilter){
+                if (hiddenFilter.hasOwnProperty(i)) {
+                    hiddenFilter[i].style.opacity = '1';
+                }
+            }
         } else {
-            document.getElementsByClassName('hiddenFilter')[0].style.opacity = '';
+            for(var i in hiddenFilter){
+                if (hiddenFilter.hasOwnProperty(i)) {
+                    hiddenFilter[i].style.opacity = '';
+                }
+            }
         }
         var articles = document.getElementsByClassName('card');
         for(var i=0, len=articles.length; i<len; i++) {
@@ -21,10 +29,14 @@
             }
         }
     };
-    window.addEventListener('hashchange', function() {
-        check();
-    });
+    
     window.addEventListener('load', function() {
-        check();
+        var hiddenFilter = document.getElementsByClassName('hiddenFilter');
+        if (hiddenFilter.length !== 0) {
+            check(hiddenFilter);
+            window.addEventListener('hashchange', function() {
+                check(hiddenFilter);
+            });
+        }
     });
 })()
